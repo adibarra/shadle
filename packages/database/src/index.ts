ead4d790b49adb929dbe650e64c4e67bb25954d4
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-11 21:50:54 -0600

    rename operations file

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,6 @@
 export * from './initializer'
 export * from './migrations'
-export * from './operations/history'
+export * from './operations/puzzle-attempts'
 export * from './operations/push-subscriptions'
 export * from './operations/puzzle-stats'
 export * from './types'
