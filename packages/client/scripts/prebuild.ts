aa2c67a5f002b9b5ffa0e19c80a745fb9f8236aa
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-17 01:27:32 -0600

    clean up prebuild script logging

--- packages/client/scripts/prebuild.ts
+++ packages/client/scripts/prebuild.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { execSync } from 'node:child_process'
 
-console.log('Generating PWA assets...')
-execSync('pnpm pwa-assets-generator public/favicon.svg -p minimal-2023', { stdio: 'inherit' })
-console.log('Prebuild complete!')
+console.log(`[${new Date().toISOString().slice(0, 19)}] INFO: [PREBUILD] Generating PWA assets...`)
+execSync('pnpm pwa-assets-generator public/favicon.svg -p minimal-2023', { stdio: 'ignore' })
+console.log(`[${new Date().toISOString().slice(0, 19)}] INFO: [PREBUILD] Finished generating PWA assets.`)
