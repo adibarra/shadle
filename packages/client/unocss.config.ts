20f05fdd0d2546b7258d67f3f2d478a2154afc4a
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-17 11:13:51 -0600

    update unocss config

--- packages/client/unocss.config.ts
+++ packages/client/unocss.config.ts
@@ -10,21 +10,14 @@ import {
 
 export default defineConfig({
   shortcuts: [
+    [/^([^.\s]+)--c-([^)\]>\s]+)$/, ([, prefix, color]) => `${prefix}-[var(--c-${color})]`],
     ['custom-link', 'text--c-accent outline-none underline-1 hover:underline focus:underline'],
     ['custom-outline', 'border--c-inverse outline-none rd-1 b-1'],
     ['custom-hover', 'hover:bg--c-secondary focus:bg--c-secondary focus:border--c-accent focus-within:border--c-accent'],
-    ['custom-btn', 'custom-outline custom-hover border--c-inverse/80'],
+    ['custom-btn', 'custom-outline custom-hover border--c-inverse-2'],
     ['custom-icon-btn', 'outline-none inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text--c-accent focus:opacity-100 focus:text--c-accent'],
     ['text-xs', 'text-[0.75rem] line-height-[1rem]'],
     ['text-xxs', 'text-[0.70rem] line-height-[0.75rem]'],
-  ],
-  rules: [
-    [/^([^.\s]+)--c-([^/)\]>\s]+)(?:\/(\d+))?$/, ([, prefix, color, opacity]) => {
-      const value = opacity ? `color-mix(in srgb, var(--c-${color}), transparent ${100 - Number.parseInt(opacity)}%)` : `var(--c-${color})`
-      return {
-        [prefix]: value,
-      }
-    }],
   ],
   presets: [
     presetWind3(),
