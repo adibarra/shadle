3aed1f8deb50cf56946069503a2121ccff2f3f20
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-20 00:45:14 -0600

    add Footer and Header components

---
+++ packages/client/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="m-2 text-center text-sm sm:text-base" role="contentinfo">
+    <span op-50>
+      Made with ❤️ by
+    </span>
+    <a
+      href="https://adibarra.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Portfolio Website"
+      aria-label="Visit adibarra's portfolio website"
+      class="custom-link font-600 op-80"
+    >
+      adibarra
+    </a>
+    <div v-if="isCompactViewport" h-4 />
+  </footer>
+</template>
---
+++ packages/client/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="m-6 text-center text-6xl">
+    <span class="text-red-500">S</span>
+    <span class="text-green-500">h</span>
+    <span class="text-blue-500">a</span>
+    <span class="text-yellow-500">d</span>
+    <span class="text-purple-500">l</span>
+    <span class="text-cyan-500">e</span>
+  </h1>
+</template>
