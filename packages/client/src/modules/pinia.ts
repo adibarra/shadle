0e18b3d4243d0641a2fd87ebeb50557bdb2d8166
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-22 21:02:33 -0600

    add comment with pinia url

--- packages/client/src/modules/pinia.ts
+++ packages/client/src/modules/pinia.ts
@@ -1,6 +1,7 @@
 import type { UserModule } from '~/types'
 import { createPinia } from 'pinia'
 
+// https://pinia.vuejs.org/
 export const install: UserModule = ({ app }) => {
   const pinia = createPinia()
   app.use(pinia)
