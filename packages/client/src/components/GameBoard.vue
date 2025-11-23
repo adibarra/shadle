e3994fbd2dfa27d0c845f166739664912b03492a
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-22 21:58:56 -0600

    add animated guess indicator

--- packages/client/src/components/GameBoard.vue
+++ packages/client/src/components/GameBoard.vue
@@ -17,8 +17,11 @@ const props = defineProps<Props>()
     <div
       v-for="(_, guessIndex) in Array(6).fill(null)"
       :key="guessIndex"
-      class="grid grid-cols-5 gap-1"
+      class="relative grid grid-cols-5 gap-1"
     >
+      <div v-if="guessIndex === props.guesses.length" class="bounce-lr absolute left-[-1.5rem] top-1/2 w-4 flex items-center justify-center">
+        <span class="color-cycle text-lg">&gt;</span>
+      </div>
       <div
         v-for="(__, colorIndex) in Array(5).fill(null)"
         :key="colorIndex"
@@ -50,3 +53,53 @@ const props = defineProps<Props>()
     </div>
   </div>
 </template>
+
+<style scoped>
+@keyframes bounce-left-right {
+  0%,
+  100% {
+    transform: translateX(0) translateY(-50%);
+  }
+  50% {
+    transform: translateX(2px) translateY(-50%);
+  }
+}
+
+@keyframes color-cycle {
+  0% {
+    color: var(--color-r);
+  }
+  12.5% {
+    color: var(--color-g);
+  }
+  25% {
+    color: var(--color-b);
+  }
+  37.5% {
+    color: var(--color-y);
+  }
+  50% {
+    color: var(--color-m);
+  }
+  62.5% {
+    color: var(--color-c);
+  }
+  75% {
+    color: var(--color-p);
+  }
+  87.5% {
+    color: var(--color-o);
+  }
+  100% {
+    color: var(--color-r);
+  }
+}
+
+.bounce-lr {
+  animation: bounce-left-right 1s ease-in-out infinite;
+}
+
+.color-cycle {
+  animation: color-cycle 15s linear infinite;
+}
+</style>
