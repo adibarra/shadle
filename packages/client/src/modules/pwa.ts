715fd872c67fa57a2a1cc2b4dd0a5ed6c80820b9
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-23 15:37:08 -0600

    auto refresh on pwa update found

--- packages/client/src/modules/pwa.ts
+++ packages/client/src/modules/pwa.ts
@@ -5,7 +5,12 @@ export const install: UserModule = ({ router }) => {
   router.isReady()
     .then(async () => {
       const { registerSW } = await import('virtual:pwa-register')
-      registerSW({ immediate: true })
+      registerSW({
+        immediate: true,
+        onNeedRefresh() {
+          window.location.reload()
+        },
+      })
     })
     .catch(() => {})
 }
