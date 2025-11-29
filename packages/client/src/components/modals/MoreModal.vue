820c4384007ec933375b45037d98adf50d8d1030
adibarra <93070681+adibarra@users.noreply.github.com>
2025-11-28 20:04:49 -0600

    remove manual update checking

--- packages/client/src/components/modals/MoreModal.vue
+++ packages/client/src/components/modals/MoreModal.vue
@@ -1,28 +1,6 @@
 <script setup lang="ts">
-import { useRegisterSW } from 'virtual:pwa-register/vue'
-
 const ui = useUiStore()
 const { t } = useI18n()
-
-let registration: ServiceWorkerRegistration | undefined
-
-useRegisterSW({
-  onRegistered(r: ServiceWorkerRegistration | undefined) {
-    registration = r
-  },
-})
-
-async function checkForUpdate() {
-  if (registration) {
-    await registration.update()
-    // eslint-disable-next-line no-alert
-    alert('Update check completed. If an update was available, it has been installed and the app will reload.')
-    window.location.reload()
-  } else {
-    // eslint-disable-next-line no-alert
-    alert('Service worker not registered.')
-  }
-}
 </script>
 
 <template>
@@ -42,12 +20,6 @@ async function checkForUpdate() {
         icon="i-carbon:document"
         :text="t('more.privacyPolicy')"
         @click="ui.open('privacyPolicy')"
-      />
-      <IconButton
-        v-if="isPwa"
-        icon="i-carbon:renew"
-        :text="t('more.checkForUpdates')"
-        @click="checkForUpdate"
       />
     </div>
   </BaseModal>
